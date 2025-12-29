6111736fd270f9d011b29918ae8beef326d3828b
Richard2919 <richardstz@bol.com.br>
2025-12-29 13:07:56 -0300

    exercicio emuneração/composição

--- enumeracaoComposicao/src/app/Main.java
+++ enumeracaoComposicao/src/app/Main.java
@@ -1,20 +1,56 @@
 package app;
 
-import entitis.Order;
-import entitisEnum.OrderStatus;
+import entitis.Department;
+import entitis.HourContract;
+import entitis.Worker;
+import entitisEnum.WorkerLevel;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-        System.out.println(order);
+        System.out.println("Enter department´s name: ");
+        String departmentsName = sc.nextLine();
+        System.out.println("Enter worker data: ");
+        System.out.println("Name: ");
+        String nameWorker = sc.nextLine();
+        System.out.println("Level: ");
+        String workerLevel = sc.nextLine();
+        System.out.println("Base salary: ");
+        Double salaryB = sc.nextDouble();
 
-        OrderStatus os1 = OrderStatus.DELIVERED;
+        Worker worker = new Worker(nameWorker, WorkerLevel.valueOf(workerLevel), salaryB, new Department(departmentsName));
 
-        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
-        System.out.println(os1);
-        System.out.println(os2);
+        System.out.println("How many contracts to this worker? ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++){
+            System.out.println("Enter contract #" + 1 + "data:");
+            System.out.print("Date (DD/MM/YYYY): ");
+            Date contractDate = sdf.parse(sc.next());
+            System.out.print("Value per hour: ");
+            double valuePerHour = sc.nextDouble();
+            System.out.print("Duration (hours): ");
+            int hours = sc.nextInt();
+            HourContract contract = new HourContract(contractDate, valuePerHour, hours);
+            worker.addContract(contract);
+        }
+
+        System.out.println();
+        System.out.print("Enter month and year to calculate income (MM/yyyy)");
+        String monthAndYear = sc.next();
+        int month = Integer.parseInt(monthAndYear.substring(0,2));
+        int year = Integer.parseInt(monthAndYear.substring(3));
+        System.out.println("Name: " + worker.getName());
+        System.out.println("Department: " + worker.getDepartment().getName());
+        System.out.println("Income for" + monthAndYear + ": " + worker.income(year, month));
+
+        sc.close();
     }
 }
---
+++ enumeracaoComposicao/src/entitisEnum/WorkerLevel.java
@@ -0,0 +1,8 @@
+package entitisEnum;
+
+public enum WorkerLevel {
+    JUNIOR,
+    MID_LEVEL,
+    SENIOR;
+}
+
